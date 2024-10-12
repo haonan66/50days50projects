85d08ac4221562bfc6000955601c311f2009edbd
haonan66 <731331417@qq.com>
2024-10-12 12:58:48 +0800

    fix: :bug: 修改day48：random-image-feed

--- src/pages/RandomImageFeed/index.tsx
+++ src/pages/RandomImageFeed/index.tsx
@@ -1,24 +1,24 @@
 import './index.scss'
 
 export default function RandomImageFeed() {
-    return (
-        <div className='random-image-wrap'>
-            <div className="main-wrap">
-                <h1>Random Image Feed</h1>
-                <div className="image-wrap">
-                    {
-                        [...Array(30)].map((_, i) => (
-                            <img
-                                key={i}
-                                src={`https://picsum.photos/seed/${Math.random()}/300/300`}
-                                alt='random image'
-                            />
-                        ))
-                    }
-                </div>
-
-            </div>
-
+  return (
+    <div className='random-image-wrap'>
+      <div className="main-wrap">
+        <h1>Random Image Feed</h1>
+        <div className="image-wrap">
+          {
+            [...Array(30)].map((_, i) => (
+              <img
+                key={i}
+                src={`https://picsum.photos/seed/${Math.random()}/300/300`}
+                alt='random image'
+              />
+            ))
+          }
         </div>
-    )
+
+      </div>
+
+    </div>
+  )
 }
